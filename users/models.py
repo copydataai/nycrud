a2ba6bfc18fb406889f95d3068989cb0d4693bac
copydataai <copydataai@gmail.com>
2021-07-15 17:08:32 -0400

    Define models and templates base.

--- users/models.py
+++ users/models.py
@@ -1,7 +1,16 @@
+"""User models."""
+
+# Django
+from django.contrib.auth.models import User
 from django.db import models
 
 class Profile(models.Model):
+    """Profile model."""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(upload_to='users/pictures', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        """Return username."""
+        return self.user.username
